c2651b3c59f33b2fedeabd8169c46ab6da5eb6a0
sherwin <sherwinlukes@gmail.com>
2020-08-23 23:41:35 +0500

    sign-in component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage.component";
 import ShopPage from "./pages/shoppage/ShopPage.component";
 import Header from "./components/header/Header.component";
+import LoginRegister from "./pages/login-and-register/LoginRegister.component";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={LoginRegister} />
       </Switch>
     </div>
   );
---
+++ src/pages/login-and-register/LoginRegister.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./login-register.styles.scss";
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const LoginRegister = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  );
+};
+
+export default LoginRegister;
