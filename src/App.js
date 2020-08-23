6d400c170e5ab4263ca0cc245cb0f55403dcb08e
sherwin <sherwinlukes@gmail.com>
2020-08-23 23:02:47 +0500

    header & logo

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/HomePage.component";
 import ShopPage from "./pages/shoppage/ShopPage.component";
+import Header from "./components/header/Header.component";
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
