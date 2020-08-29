68fdf55af4654ec59b0d195c8b2c9715818ce5e9
sherwin <sherwinlukes@gmail.com>
2020-08-29 21:49:41 +0500

    email password sign-up

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ class App extends React.Component {
             },
           });
         });
+        console.log(this.state);
       } else {
         this.setState({ currentUser: userAuth });
       }
--- src/pages/login-and-register/LoginRegister.component.jsx
+++ src/pages/login-and-register/LoginRegister.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./login-register.styles.scss";
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const LoginRegister = () => {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
