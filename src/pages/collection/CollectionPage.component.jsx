d889648653b86603d3d87746e0182541f0715698
sherwin <sherwinlukes@gmail.com>
2020-09-14 22:44:45 +0500

    making nested routes for collections

---
+++ src/pages/collection/CollectionPage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./collection-page.styles.scss";
+import CollectionItem from "../../components/collection-item/CollectionItem.component";
+
+const CategoryPage = ({ match }) => {
+  return (
+    <div className="category">
+      <h2>Category Page</h2>
+    </div>
+  );
+};
+
+export default CategoryPage;
--- src/pages/shoppage/ShopPage.component.jsx
+++ src/pages/shoppage/ShopPage.component.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import CollectionsOverview from "../../components/collections-overview/CollectionsOverview.component";
+import { Route } from "react-router-dom";
 
-const ShopPage = ({ collections }) => {
+import CollectionsOverview from "../../components/collections-overview/CollectionsOverview.component";
+import CollectionPage from "../collection/CollectionPage.component";
+
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
-      <CollectionsOverview />
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:categoryId`} component={CollectionPage} />
     </div>
   );
 };
